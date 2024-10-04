1c9f9141255c9f92e797fcbc08d7e68847c15754
Shahrizal <111113150+ShahrizalEffendy@users.noreply.github.com>
2024-10-04 09:58:12 +0800

    Add files via upload

---
+++ storage/framework/views/48000bc6d93e21ec8d8975abb0a75f87.php
@@ -0,0 +1,6 @@
+
+
+<?php $__env->startSection('content'); ?>
+    <?php echo $__env->make('form',['task'=>$task], \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\HP 14S\laravel course\task-list\resources\views/edit.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/6052984e4a17bdc0101b7cd971155b6b.php
@@ -0,0 +1,6 @@
+
+
+<?php $__env->startSection('content'); ?>
+    <?php echo $__env->make('form', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\HP 14S\laravel course\task-list\resources\views/create.blade.php ENDPATH**/ ?>
